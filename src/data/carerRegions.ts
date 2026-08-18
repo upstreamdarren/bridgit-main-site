96c1a2d00095d9c2cb1b083011fb04974144b165
Darren <38461961+upstreamdarren@users.noreply.github.com>
2026-08-18 20:43:55 +0100

    Expand user portal content and local support

--- src/data/carerRegions.ts
+++ src/data/carerRegions.ts
@@ -26,13 +26,13 @@ export const carerRegions: CarerRegion[] = [
   { area: "Ealing", organisation: "Carers Trust Ealing", href: "https://carers.bridgit.care/app/ealing/ca", logo: "/images/clients/ealing-council.jpg", x: 58, y: 72 },
   { area: "Gloucestershire (South)", organisation: "South Gloucestershire Council", href: "https://carers.bridgit.care/app/sglo/ca", logo: "/images/clients/south-gloucestershire-council.png", x: 42, y: 70 },
   { area: "Hammersmith & Fulham", organisation: "Carers Network", href: "https://carers.bridgit.care/app/cnet/ca", logo: "/images/clients/carers-network.png", x: 60, y: 73 },
-  { area: "Harrow", organisation: "Harrow Carers", href: "https://carers.bridgit.care/app/hcar/ca", x: 58, y: 69 },
-  { area: "Hartlepool", organisation: "Hartlepool Carers", href: "https://carers.bridgit.care/app/hplc/ca", x: 61, y: 29 },
-  { area: "Hillingdon", organisation: "Carers Trust Hillingdon", href: "https://carers.bridgit.care/app/cthil/ca", x: 57, y: 72 },
+  { area: "Harrow", organisation: "Harrow Carers", href: "https://carers.bridgit.care/app/hcar/ca", logo: "/images/clients/harrow-carers.png", x: 58, y: 69 },
+  { area: "Hartlepool", organisation: "Hartlepool Carers", href: "https://carers.bridgit.care/app/hplc/ca", logo: "/images/clients/hartlepool-carers.webp", x: 61, y: 29 },
+  { area: "Hillingdon", organisation: "Carers Trust Hillingdon", href: "https://carers.bridgit.care/app/cthil/ca", logo: "/images/clients/carers-trust-hillingdon.webp", x: 57, y: 72 },
   { area: "Inverclyde", organisation: "Unity Enterprise", href: "https://carers.bridgit.care/app/cginv/ca", logo: "/images/clients/inverclyde-council.png", x: 32, y: 16 },
   { area: "Lincolnshire (North)", organisation: "North Lincolnshire Council", href: "https://carers.bridgit.care/app/cssnl/ca", logo: "/images/clients/north-lincolnshire-council.svg", x: 61, y: 45 },
   { area: "Lincolnshire (North East)", organisation: "North East Lincolnshire Council", href: "https://carers.bridgit.care/app/cssnel/ca", logo: "/images/clients/north-east-lincolnshire-council.svg", x: 65, y: 43 },
-  { area: "Newcastle", organisation: "Newcastle Carers", href: "#area-help", x: 58, y: 27, comingSoon: true },
+  { area: "Newcastle", organisation: "Newcastle Carers", href: "#area-help", logo: "/images/clients/newcastle-carers.svg", x: 58, y: 27, comingSoon: true },
   { area: "Northamptonshire", organisation: "Northamptonshire Carers", href: "https://carers.bridgit.care/app/ncass/ca", logo: "/images/clients/north-northamptonshire-council.png", x: 57, y: 57 },
   { area: "Peterborough", organisation: "Peterborough City Council", href: "https://carers.bridgit.care/app/ptbc/ca", logo: "/images/clients/peterborough-council.png", x: 62, y: 53 },
   { area: "Plymouth", organisation: "Plymouth City Council", href: "https://carers.bridgit.care/devon/", logo: "/images/clients/plymouth-city-council.svg", x: 29, y: 92 },
@@ -40,6 +40,6 @@ export const carerRegions: CarerRegion[] = [
   { area: "Solihull", organisation: "Carers Trust", href: "https://carers.bridgit.care/app/ctsol/ca", logo: "/images/clients/solihull-council.png", x: 49, y: 62 },
   { area: "Somerset", organisation: "Thrive Somerset", href: "https://carers.bridgit.care/app/somerset/ca", logo: "/images/clients/thrive.png", x: 38, y: 78 },
   { area: "Torbay", organisation: "Torbay Council", href: "https://carers.bridgit.care/devon/", logo: "/images/clients/torbay-council.webp", x: 32, y: 90 },
-  { area: "Warwickshire", organisation: "Warwickshire County Council", href: "https://bridgit.care/support/warwickshire", x: 51, y: 62 },
-  { area: "Wolverhampton", organisation: "Wolverhampton City Council", href: "#area-help", x: 46, y: 58, comingSoon: true }
+  { area: "Warwickshire", organisation: "Warwickshire County Council", href: "https://bridgit.care/support/warwickshire", logo: "/images/clients/warwickshire-county-council.svg", x: 51, y: 62 },
+  { area: "Wolverhampton", organisation: "Wolverhampton City Council", href: "#area-help", logo: "/images/clients/wolverhampton-city-council.svg", x: 46, y: 58, comingSoon: true }
 ];
---
+++ src/data/youngCarerRegions.ts
@@ -0,0 +1,16 @@
+import type { CarerRegion } from "./carerRegions";
+
+export const youngCarerRegions: CarerRegion[] = [
+  { area: "Ayrshire (North)", organisation: "Unity Enterprise", href: "https://young.bridgit.care/app/cgna/ca", logo: "/images/clients/unity-enterprise.png", x: 30, y: 16 },
+  { area: "Ayrshire (South)", organisation: "Unity Enterprise", href: "https://young.bridgit.care/app/cgsa/ca", logo: "/images/clients/unity-enterprise.png", x: 29, y: 22 },
+  { area: "Brent", organisation: "Brent Carers Centre", href: "#area-help", logo: "/images/clients/brent-carers.png", x: 59, y: 70, comingSoon: true },
+  { area: "Camden", organisation: "Camden Carers", href: "#area-help", logo: "/images/clients/camden-carers.jpeg", x: 61, y: 72, comingSoon: true },
+  { area: "Fife", organisation: "Fife Young Carers", href: "https://young.bridgit.care/app/fyc/ca", logo: "/images/clients/fife-young-carers.svg", x: 42, y: 13 },
+  { area: "Gloucestershire (South)", organisation: "South Gloucestershire Council", href: "#area-help", logo: "/images/clients/south-gloucestershire-council.png", x: 42, y: 70, comingSoon: true },
+  { area: "Harrow", organisation: "Harrow Carers", href: "#area-help", logo: "/images/clients/harrow-carers.png", x: 58, y: 69, comingSoon: true },
+  { area: "Hillingdon", organisation: "Carers Trust Hillingdon", href: "#area-help", logo: "/images/clients/carers-trust-hillingdon.webp", x: 57, y: 72, comingSoon: true },
+  { area: "Inverclyde", organisation: "Unity Enterprise", href: "https://young.bridgit.care/app/cginv/ca", logo: "/images/clients/unity-enterprise.png", x: 32, y: 16 },
+  { area: "Newcastle", organisation: "Newcastle Carers", href: "#area-help", logo: "/images/clients/newcastle-carers.svg", x: 58, y: 27, comingSoon: true },
+  { area: "Northamptonshire", organisation: "Northamptonshire Carers", href: "#area-help", logo: "/images/clients/north-northamptonshire-council.png", x: 57, y: 57, comingSoon: true },
+  { area: "Plymouth", organisation: "Plymouth City Council", href: "#area-help", logo: "/images/clients/plymouth-city-council.svg", x: 29, y: 92, comingSoon: true }
+];
